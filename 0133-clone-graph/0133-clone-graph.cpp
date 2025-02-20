0f5c20a8c672b9bdfdc4777ebedd8d3f56430ee6
fweilun <william20020503@gmail.com>
2025-02-20 22:59:59 +0800

    Time: 7 ms (22.53%), Space: 12.2 MB (46.83%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,23 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class Solution {
 public:
+    vector<Node*> vis;
+    void dfs(Node* node, Node* out){
+        if (node==nullptr) return;
+        out->val = node->val;
+        vis[node->val] = out;
+        for (Node* child:node->neighbors){
+            if (vis[child->val]) {
+                out->neighbors.push_back(vis[child->val]);
+                continue;
+            }
+            Node* cpy_child = new Node();
+            out->neighbors.push_back(cpy_child);
+            dfs(child, cpy_child);
+        }
+    }
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*, Node*> oldtonew;
-        return dfs(node, oldtonew);
+        if (!node) return nullptr;
+        Node* out = new Node();
+        vis = vector<Node*>(101, nullptr);
+        dfs(node, out);
+        return out;
     }
 
-    Node* dfs(Node* node, unordered_map<Node*, Node*>& oldtonew) {
-        if (!node) {
-            return nullptr;
-        }
-        if (oldtonew.count(node)) {
-            return oldtonew[node];
-        }
-        Node* copy = new Node(node->val);
-        oldtonew[node] = copy;
-        for (Node* n : node->neighbors) {
-            copy->neighbors.push_back(dfs(n, oldtonew));
-        }
-        return copy;
-        
-    }
 };

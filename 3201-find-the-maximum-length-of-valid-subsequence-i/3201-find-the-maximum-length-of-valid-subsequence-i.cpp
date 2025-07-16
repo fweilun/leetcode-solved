91cbe3e8c9fcdd05098662940a60053d33d150e3
fweilun <william20020503@gmail.com>
2025-07-16 13:52:06 +0800

    Time: 4 ms (65.29%), Space: 96.2 MB (40%) - LeetHub

---
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int temp=-1;
+        int out=0, zeros=0, ones=0;
+        for (int& x:nums) {
+            x%=2;
+            if (x!=temp) {
+                temp=x;
+                ++out;
+            }
+            ones+=x;
+        }
+        zeros = nums.size() - ones;
+        
+        return max(out, max(zeros, ones));
+    }
+};
